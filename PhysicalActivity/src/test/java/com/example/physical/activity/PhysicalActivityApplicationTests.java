8f1f109ce4f3f919287f4dd71f12abd7abab4a03
Josip Bošnjak <jbosnjak@unipu.hr>
2017-07-04 11:23:32 +0200

    sustav za pracenje fizicke aktivnosti dodan

---
+++ PhysicalActivity/src/test/java/com/example/physical/activity/PhysicalActivityApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.physical.activity;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PhysicalActivityApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
