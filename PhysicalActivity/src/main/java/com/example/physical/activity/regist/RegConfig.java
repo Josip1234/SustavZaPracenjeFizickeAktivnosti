248f9e1c1b6dc75356afeef87938dcb195dfce08
Josip Bošnjak <jbosnjak@unipu.hr>
2017-07-07 10:36:19 +0200

    kreirane klase za izračun tjelesnog indeksa

--- PhysicalActivity/src/main/java/com/example/physical/activity/regist/RegConfig.java
+++ PhysicalActivity/src/main/java/com/example/physical/activity/regist/RegConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 import com.example.physical.activity.RegTest;
+
 import com.example.physical.activity.login.test;
 
 @Configuration
