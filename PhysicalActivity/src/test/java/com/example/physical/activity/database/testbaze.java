02bf8088ce78823d9aba624a09468cf3159b9900
Josip Bošnjak <jbosnjak@unipu.hr>
2017-07-09 16:00:18 +0200

    napravljena veza prema bazi
    napravljena je veza prema bazi sada je potrebno kreirati klasu za upite
    i povezati registraciju i login sa klasom koja sadrži vezu sa bazom

---
+++ PhysicalActivity/src/test/java/com/example/physical/activity/database/testbaze.java
@@ -0,0 +1,13 @@
+package com.example.physical.activity.database;
+
+
+
+public class testbaze {
+
+	public static void main(String[] args) {
+		PhysicalActivityDatabase baza = new PhysicalActivityDatabase();
+		
+		baza.spoji();
+	}
+
+}
