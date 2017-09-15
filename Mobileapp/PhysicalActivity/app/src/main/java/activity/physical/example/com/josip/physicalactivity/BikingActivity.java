4949bbb96401058fd5a3db6f72c9b23d7cd0b780
Josip Bošnjak <jbosnjak@unipu.hr>
2017-09-15 19:40:56 +0200

    NA bike activity je napravljeno sve kao i na trčanje aktivity dodan je button koji otvara google mapu gdje bi korisnik mogao crtati svoju rutu i spremati je u bazu ili pregledavati trenutnu rutu i mijenjati je

--- Mobileapp/PhysicalActivity/app/src/main/java/activity/physical/example/com/josip/physicalactivity/BikingActivity.java
+++ Mobileapp/PhysicalActivity/app/src/main/java/activity/physical/example/com/josip/physicalactivity/BikingActivity.java
@@ -1,13 +1,336 @@
 package activity.physical.example.com.josip.physicalactivity;
 
+import android.content.Intent;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Chronometer;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 public class BikingActivity extends AppCompatActivity {
+private Chronometer cron;
+private Button mStart;
+private Button mStop;
+private Button mReset;
+    private TextView mSpeed;
+    private TextView mSpeedkmH;
+    private TextView mUlica;
+    private TextView mPostaNaziv;
+    private TextView mDrzava;
+    private TextView mKorisnik;
+    private TextView mUdaljenost;
+private Button mGoogleMaps;
+
+    public void startcron(){
+        cron=(Chronometer) findViewById(R.id.chronometer4);
+        cron.start();
+    }
+    public void stopcron(){
+        cron=(Chronometer) findViewById(R.id.chronometer4);
+        cron.stop();
+    }
+    public void resetcron(){
+        cron=(Chronometer) findViewById(R.id.chronometer4);
+        cron.setBase(SystemClock.elapsedRealtime());
+        cron.stop();
+    }
+    public String vrati_korisnika() throws IOException,JSONException {
+        String naziv="prijava.json";
+        String korisnik="";
+        FileInputStream fis = openFileInput(naziv);
+        BufferedInputStream bis = new BufferedInputStream(fis);
+        StringBuffer b = new StringBuffer();
+        while (bis.available() !=0){
+            char c = (char) bis.read();
+            b.append(c);
+        }
+        bis.close();
+        fis.close();
+
+        JSONArray data = new JSONArray(b.toString());
+        StringBuffer prijavaBuffer= new StringBuffer();
+        for(int i=0;i<data.length();i++){
+            String object=data.getJSONObject(i).getString("username");
+            korisnik=object;
+            prijavaBuffer.append(object);
+        }
+        Log.i("poruka","pročitan json");
+        Log.i("korisnik",korisnik);
+        return korisnik;
+
+
+    }
+    private static double distance(double lat1, double lon1, double lat2, double lon2, String unit) {
+        double theta = lon1 - lon2;
+        double dist = Math.sin(deg2rad(lat1)) * Math.sin(deg2rad(lat2)) + Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * Math.cos(deg2rad(theta));
+        dist = Math.acos(dist);
+        dist = rad2deg(dist);
+        dist = dist * 60 * 1.1515;
+        if (unit == "K") {
+            dist = dist * 1.609344;
+        } else if (unit == "N") {
+            dist = dist * 0.8684;
+        }
+
+        return (dist);
+    }
+
+    /*:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
+	/*::	This function converts decimal degrees to radians						 :*/
+	/*:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
+    private static double deg2rad(double deg) {
+        return (deg * Math.PI / 180.0);
+    }
+
+    /*:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
+	/*::	This function converts radians to decimal degrees						 :*/
+	/*:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
+    private static double rad2deg(double rad) {
+        return (rad * 180 / Math.PI);
+    }
+
+    public void kreiraj_prethodne_koordinate(double lat ,double lon) throws IOException,JSONException{
+
+        JSONArray array = new JSONArray();
+        JSONObject object;
+        object=new JSONObject();
+        object.put("koordinata",lat);
+        array.put(object);
+        object=new JSONObject();
+        object.put("koordinata",lon);
+        array.put(object);
+        String text = array.toString();
+        FileOutputStream fos = openFileOutput("koordinate.json",MODE_PRIVATE);
+        fos.write(text.getBytes());
+        fos.close();
+        Log.i("message","succesfully written to json");
+    }
+
+
+    public String vrati_koordinate(int pozicija) throws IOException,JSONException {
+        String naziv="koordinate.json";
+
+
+        FileInputStream fis = openFileInput(naziv);
+        BufferedInputStream bis = new BufferedInputStream(fis);
+        StringBuffer b = new StringBuffer();
+        while (bis.available() !=0){
+            char c = (char) bis.read();
+            b.append(c);
+        }
+        bis.close();
+        fis.close();
+
+        JSONArray data = new JSONArray(b.toString());
+        StringBuffer prijavaBuffer = new StringBuffer();
+
+
+        String koordinata2 = data.getJSONObject(pozicija).getString("koordinata");
+        prijavaBuffer.append(koordinata2);
+
+
+
+
+
+
+
+
+        Log.i("poruka","pročitan json");
+
+        return prijavaBuffer.toString();
+
+
+    }
+
+
+    public void dohvati_koordinate(double lat,double lon){
+
+
+        try {
+            kreiraj_prethodne_koordinate(lat,lon);
+            Log.i("poruka","Uspješno spremljene koordinate");
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+    public void idi_na_mapu(){
+        Intent intent = new Intent(BikingActivity.this,MapsActivity.class);
+        startActivity(intent);
+    }
+    LocationListener locListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(Location location) {
+            if(location!=null) {
+                mSpeed=(TextView) findViewById(R.id.brzina);
+                mSpeedkmH=(TextView) findViewById(R.id.brzinakm);
+                mUlica=(TextView) findViewById(R.id.ulica);
+                mPostaNaziv=(TextView) findViewById(R.id.postbrngr);
+                mDrzava=(TextView) findViewById(R.id.drzava);
+                mKorisnik=(TextView) findViewById(R.id.korisnik);
+                mUdaljenost=(TextView) findViewById(R.id.udaljenost);
+                mGoogleMaps=(Button) findViewById(R.id.mapa);
+                mGoogleMaps.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        idi_na_mapu();
+                    }
+                });
+                double loc1 = 00.00;
+                double loc2 = 00.00;
+                try {
+                    loc1 = Double.parseDouble(vrati_koordinate(0));
+                    loc2 = Double.parseDouble(vrati_koordinate(1));
+                    System.out.println(loc1);
+                    System.out.println(loc2);
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                double loc3 =  location.getLatitude();
+                double loc4 =  location.getLongitude();
+                System.out.println(loc3);
+                System.out.println(loc4);
+                double distance = distance(loc1,loc2,loc3,loc4,"K");
+
+                System.out.println("Udaljenost do druge točke:" + String.valueOf(distance) + " kilometara");
+                try {
+
+                    mUdaljenost.setText("Udaljenost:" + String.valueOf(distance) + "km");
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+
+                try {
+                    mKorisnik.setText(vrati_korisnika());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                dohvati_koordinate(loc3, loc4);
+                String cityName = null;
+                String stateName = null;
+                String ad = null;
+                Geocoder gcd = new Geocoder(getBaseContext(), Locale.getDefault());
+                List<Address> addresses;
+
+
+
+
+                try {
+                    addresses = gcd.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+                    if (addresses.size() > 0) {
+                        try {
+                            Log.i("poruka", addresses.get(0).getLocality());
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                        Log.i("poruka", addresses.get(0).getCountryName());
+                        Log.i("poruka", addresses.get(0).getAddressLine(0));
+                        try {
+                            Log.i("poruka", addresses.get(0).getAddressLine(1));
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                        try {
+                            Log.i("poruka", addresses.get(0).getAddressLine(2));
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                        cityName = addresses.get(0).getLocality();
+                        stateName = addresses.get(0).getCountryName();
+                        ad = addresses.get(0).getAddressLine(0);
+                        float trenutna_brzina = location.getSpeed();
+                        float brzina = (float) (trenutna_brzina * 3.6);
+                        mSpeed.setText(String.valueOf(trenutna_brzina) + " m/s");
+                        mSpeedkmH.setText(String.valueOf(brzina) + " km/h");
+
+
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+
+                String s =
+                        cityName;
+                String p = stateName;
+                String a = ad;
+
+
+                mUlica.setText(a);
+
+                mPostaNaziv.setText(s);
+
+                mDrzava.setText(p);
+                location.reset();
+
+            }
+        };
+
+        @Override
+        public void onStatusChanged(String s, int i, Bundle bundle) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String s) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String s) {
+
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_biking);
+        mStart=(Button) findViewById(R.id.startch);
+        mStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startcron();
+            }
+        });
+        mStop=(Button) findViewById(R.id.stopch);
+        mStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                stopcron();
+            }
+        });
+        mReset=(Button) findViewById(R.id.resetch);
+        mReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                resetcron();
+            }
+        });
     }
 }
