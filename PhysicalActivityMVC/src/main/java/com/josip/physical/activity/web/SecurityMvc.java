9f5947f50a66e01f67727bda945da9b106bac061
Josip Bošnjak <jbosnjak@unipu.hr>
2017-12-13 15:05:29 +0100

    logiranje putem baze
    napravljeno umjesto da se logira putem memorijske autentifikacije
    korisnik se autentificira pomoću  baze, tj izvora podataka.

--- PhysicalActivityMVC/src/main/java/com/josip/physical/activity/web/SecurityMvc.java
+++ PhysicalActivityMVC/src/main/java/com/josip/physical/activity/web/SecurityMvc.java
@@ -1,6 +1,11 @@
 package com.josip.physical.activity.web;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
@@ -8,15 +13,29 @@ import org.springframework.security.config.annotation.web.servlet.configuration.
 @Configuration
 @EnableWebMvcSecurity
 public class SecurityMvc extends WebSecurityConfigurerAdapter{
-
+	@Bean
+	public DataSource dataSource() {
+		DriverManagerDataSource ds = new DriverManagerDataSource();
+		ds.setDriverClassName("com.mysql.jdbc.Driver");
+		ds.setUrl("jdbc:mysql://127.0.0.1/physicalactivity");
+		ds.setUsername("root");
+		ds.setPassword("");
+		return ds;
+	}
+	@Autowired
+	DataSource dataSource;
+	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth)
 	throws Exception {
+		/*
 	auth
 	.inMemoryAuthentication()
 	.withUser("user").password("user").roles("USER").and()
 	.withUser("admin").password("admin").roles("USER", "ADMIN");
 	}
-	
+	*/
+		auth.jdbcAuthentication().dataSource(dataSource).usersByUsernameQuery("select email,sifra,true "+ "from registration where email=?").authoritiesByUsernameQuery("select email,'ROLE_USER' from registration where email=?");
+}
 }
 
