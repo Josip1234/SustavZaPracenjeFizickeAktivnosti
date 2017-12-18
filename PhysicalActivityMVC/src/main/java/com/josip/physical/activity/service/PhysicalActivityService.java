6026ae6f0d0496e10027a9165b562caa5324d9b2
Josip Bošnjak <jbosnjak@unipu.hr>
2017-12-18 15:01:28 +0100

    Revert "Update"
    This reverts commit 1d35767ef43f382c85f6c8454921ba0f33815019.

---
+++ PhysicalActivityMVC/src/main/java/com/josip/physical/activity/service/PhysicalActivityService.java
@@ -0,0 +1,10 @@
+package com.josip.physical.activity.service;
+
+import java.util.List;
+
+import com.josip.physical.activity.login.Login;
+
+public interface PhysicalActivityService {
+public List<Login> getListOfUsers();
+
+}
