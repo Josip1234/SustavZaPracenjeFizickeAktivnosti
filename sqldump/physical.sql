b64f1847935288ec7ce7fd7c499aa20ca78f647a
Josip Bošnjak <jbosnjak@unipu.hr>
2018-02-06 10:36:04 +0100

    update baze
    Update baze: npromijenjen naziv koristiti će se naziv physical umjesto physicalactivity

--- sqldump/physical.sql
+++ sqldump/physical.sql
@@ -1,13 +1,15 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.5.2
+-- version 4.7.4
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 09, 2017 at 09:17 AM
--- Server version: 10.1.21-MariaDB
--- PHP Version: 7.1.1
+-- Generation Time: Feb 06, 2018 at 10:34 AM
+-- Server version: 10.1.30-MariaDB
+-- PHP Version: 7.2.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -17,7 +19,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `physicalactivity`
+-- Database: `physical`
 --
 
 -- --------------------------------------------------------
@@ -89,7 +91,11 @@ CREATE TABLE `registration` (
 
 INSERT INTO `registration` (`id`, `OIB`, `ime`, `prezime`, `spol`, `datumr`, `email`, `sifra`) VALUES
 (1, '86052601428', 'Josip', 'Bošnjak', 'm', '1992-11-05', 'jbosnjak3@gmail.com', 'test'),
-(2, '14588899666', 'Marko', 'Marković', 'm', '2017-09-30', 'mmarkovic@gmail.com', 'egeggheghdhehe');
+(2, '14588899666', 'Marko', 'Marković', 'm', '2017-09-30', 'mmarkovic@gmail.com', 'egeggheghdhehe'),
+(3, '12344444444', 'Bo??njak', 'bo??njak', 'm', '2017-12-12', 'aaa@gg.aa', 'sggsÄ?Ä‡Ä‡'),
+(7, '1234444444', 'Bošnjak', 'bošnjak', 'm', '2017-12-12', 'aaahh@gg.aa', 'šššš'),
+(9, '14555555555', 'Josip', 'BoÅ¡njak', 'm', '2018-02-09', 'bbosnjak@gg.gg', 'FEWFWFWEFFWFW'),
+(11, '14777777777', 'fefwefwf', 'wfgrrrgrggrÄ?Å¡', 'm', '2018-02-16', 'fefwef@gg.geg', 'fefwffeewfeff');
 
 --
 -- Indexes for dumped tables
@@ -137,21 +143,25 @@ ALTER TABLE `registration`
 --
 ALTER TABLE `activities`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
 --
 -- AUTO_INCREMENT for table `ibm`
 --
 ALTER TABLE `ibm`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
 --
 -- AUTO_INCREMENT for table `mapiranje`
 --
 ALTER TABLE `mapiranje`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
 --
 -- AUTO_INCREMENT for table `registration`
 --
 ALTER TABLE `registration`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
 --
 -- Constraints for dumped tables
 --
@@ -173,6 +183,7 @@ ALTER TABLE `ibm`
 --
 ALTER TABLE `mapiranje`
   ADD CONSTRAINT `oib` FOREIGN KEY (`OIB`) REFERENCES `registration` (`OIB`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
