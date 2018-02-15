cac514bfc08b2a8564189ec6952386154fca1f74
Josip Bošnjak <jbosnjak@unipu.hr>
2018-02-15 11:41:22 +0100

    Izračun dijelova za ukupno
    Izračun za ukupnu i prosječnu brzinu ukupan broj koraka ukupnu prijeđenu udaljenost će se računati preko interface-a izračun i preko njegove implementacije iz jsona će se vaditi rezultati. Na kraju će se to prikazivati na summary aktivnost za trenutni dan i preko imleentacije će se također slati na server.

---
+++ Mobileapp/PhysicalActivity/app/src/main/java/activity/physical/example/com/josip/physicalactivity/implementations/ImplemetacijaIzracuna.java
@@ -0,0 +1,25 @@
+package activity.physical.example.com.josip.physicalactivity.implementations;
+
+import activity.physical.example.com.josip.physicalactivity.model.Izracun;
+
+/**
+ * Created by Korisnik on 15.2.2018..
+ */
+
+public class ImplemetacijaIzracuna implements Izracun {
+    @Override
+    public boolean izracunUkupneProsjecneBrzine() {
+        
+        return false;
+    }
+
+    @Override
+    public boolean izracunUkupneUdaljenosti() {
+        return false;
+    }
+
+    @Override
+    public boolean izracunUkupnogBrojaKoraka() {
+        return false;
+    }
+}
---
+++ Mobileapp/PhysicalActivity/app/src/main/java/activity/physical/example/com/josip/physicalactivity/model/Izracun.java
@@ -0,0 +1,12 @@
+package activity.physical.example.com.josip.physicalactivity.model;
+
+/**
+ * Created by Korisnik on 15.2.2018..
+ */
+
+public interface Izracun {
+    public boolean izracunUkupneProsjecneBrzine();
+    public boolean izracunUkupneUdaljenosti();
+    public boolean izracunUkupnogBrojaKoraka();
+
+}
