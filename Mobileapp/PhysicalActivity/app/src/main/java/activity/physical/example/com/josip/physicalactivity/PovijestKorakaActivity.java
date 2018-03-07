2c6ab7d5b74a4a63c86264d88e87a31f67376641
Josip Bošnjak <jbosnjak@unipu.hr>
2018-03-07 01:37:27 +0100

    update

---
+++ Mobileapp/PhysicalActivity/app/src/main/java/activity/physical/example/com/josip/physicalactivity/PovijestKorakaActivity.java
@@ -0,0 +1,13 @@
+package activity.physical.example.com.josip.physicalactivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PovijestKorakaActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_povijest_koraka);
+    }
+}
