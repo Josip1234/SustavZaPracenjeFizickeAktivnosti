99c64435ced32580853460db13ec0c87db588526
Josip Bošnjak <jbosnjak@unipu.hr>
2018-03-08 12:33:49 +0100

    Revert "Revert "sredđena brzina?""
    This reverts commit cfc80ebaee70fba6b012282a309a172c9cf6884e.

---
+++ Mobileapp/PhysicalActivity/app/src/main/java/activity/physical/example/com/josip/physicalactivity/models/AccelerometerData.java
@@ -0,0 +1,12 @@
+package activity.physical.example.com.josip.physicalactivity.models;
+
+
+public class AccelerometerData {
+
+    public double value;
+    public float x;
+    public float y;
+    public float z;
+    public long time;
+    public boolean isRealPeak = true;
+}
