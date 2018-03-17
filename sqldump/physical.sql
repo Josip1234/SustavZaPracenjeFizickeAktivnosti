332b262e7c9e21f93737abf2b70ae2879487e980
Josip Bošnjak <jbosnjak@unipu.hr>
2018-03-17 16:27:41 +0100

    dodan dump baze

--- sqldump/physical.sql
+++ sqldump/physical.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 16, 2018 at 09:29 PM
+-- Generation Time: Mar 17, 2018 at 04:26 PM
 -- Server version: 10.1.30-MariaDB
 -- PHP Version: 7.2.1
 
@@ -43,7 +43,8 @@ CREATE TABLE `biking` (
 
 INSERT INTO `biking` (`id`, `vrijemeAktivnosti`, `brzinaUkm`, `udaljenost`, `email`, `datum`) VALUES
 (1, '0:44', 14, 1.7732136200063726, 'jbosnjak3@gmail.com', '2018-03-16 09:47:58'),
-(2, '0:53', 14, 1.7732136200063726, 'jbosnjak3@gmail.com', '2018-03-16 09:48:06');
+(2, '0:53', 14, 1.7732136200063726, 'jbosnjak3@gmail.com', '2018-03-16 09:48:06'),
+(3, '0:30', 22, 1.7732136200063726, 'jbosnjak3@gmail.com', '2018-03-17 08:15:19');
 
 -- --------------------------------------------------------
 
@@ -59,6 +60,15 @@ CREATE TABLE `bikingstatistika` (
   `prosjecnaBrzinaUkm` double NOT NULL,
   `period` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_croatian_ci;
+
+--
+-- Dumping data for table `bikingstatistika`
+--
+
+INSERT INTO `bikingstatistika` (`id`, `korisnikBike`, `ukupnaUdaljenost`, `ukupnoVrijemeAktivnosti`, `prosjecnaBrzinaUkm`, `period`) VALUES
+(1, 'jbosnjak3@gmail.com', 7.096578215918581, 34979, 11, '2018-03-17 08:15:23'),
+(2, 'jbosnjak3@gmail.com', 7.096578215918581, 34979, 11, '2018-03-17 08:15:23'),
+(3, 'jbosnjak3@gmail.com', 7.096578215918581, 34979, 11, '2018-03-17 08:15:23');
 
 -- --------------------------------------------------------
 
@@ -102,6 +112,13 @@ CREATE TABLE `running` (
   `datum` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_croatian_ci;
 
+--
+-- Dumping data for table `running`
+--
+
+INSERT INTO `running` (`id`, `vrijemeAktivnosti`, `brzinaUkm`, `udaljenost`, `email`, `datum`) VALUES
+(1, '0:28', 12, 1.7733909413678197, 'jbosnjak3@gmail.com', '2018-03-17 08:14:41');
+
 -- --------------------------------------------------------
 
 --
@@ -117,6 +134,14 @@ CREATE TABLE `runningstatistika` (
   `period` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_croatian_ci;
 
+--
+-- Dumping data for table `runningstatistika`
+--
+
+INSERT INTO `runningstatistika` (`id`, `korisnikRun`, `ukupnaUdaljenost`, `ukupnoVrijemeAktivnosti`, `prosjecnaBrzinaUkm`, `period`) VALUES
+(1, 'jbosnjak3@gmail.com', 3.550150988612203, 32949, 6, '2018-03-17 08:14:45'),
+(2, 'jbosnjak3@gmail.com', 3.550150988612203, 32949, 6, '2018-03-17 08:14:45');
+
 -- --------------------------------------------------------
 
 --
@@ -126,10 +151,21 @@ CREATE TABLE `runningstatistika` (
 CREATE TABLE `summarystatistika` (
   `id` int(11) NOT NULL,
   `korisnikSum` varchar(255) COLLATE utf8_croatian_ci NOT NULL,
+  `ukupanBrojKoraka` int(11) NOT NULL,
   `ukupnoVrijemeAktivnosti` double NOT NULL,
   `ukupnaPrijedjenaUdaljenost` double NOT NULL,
-  `prosjecnaBrzina` double NOT NULL
+  `prosjecnaBrzina` double NOT NULL,
+  `datum` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_croatian_ci;
+
+--
+-- Dumping data for table `summarystatistika`
+--
+
+INSERT INTO `summarystatistika` (`id`, `korisnikSum`, `ukupanBrojKoraka`, `ukupnoVrijemeAktivnosti`, `ukupnaPrijedjenaUdaljenost`, `prosjecnaBrzina`, `datum`) VALUES
+(1, 'jbosnjak3@gmail.com', 3, 123821, 12.423489251775166, 6.4444444444444455, '2018-03-17 13:24:09'),
+(4, 'jbosnjak3@gmail.com', 3, 123821, 12.423489251775166, 6.4444444444444455, '2018-03-17 15:17:11'),
+(5, 'jbosnjak3@gmail.com', 1, 87022, 10.646729204530784, 5.666666666666667, '2018-03-17 15:19:18');
 
 -- --------------------------------------------------------
 
@@ -169,7 +205,9 @@ INSERT INTO `walking` (`id`, `udaljenost`, `vrijemeAktivnosti`, `koraci`, `brzin
 (24, 0, '0:00', 1, 0, 'jbosnjak3@gmail.com', '2018-03-15 10:11:03'),
 (25, 106.39270311141873, '1:24', 1, 8, 'jbosnjak3@gmail.com', '2018-03-15 15:19:36'),
 (26, 106.39270311141873, '1:45', 6, 8, 'jbosnjak3@gmail.com', '2018-03-15 15:19:56'),
-(27, 106.39270311141873, '1:48', 6, 8, 'jbosnjak3@gmail.com', '2018-03-15 15:19:59');
+(27, 106.39270311141873, '1:48', 6, 8, 'jbosnjak3@gmail.com', '2018-03-15 15:19:59'),
+(28, 1.7767600472443834, '0:49', 3, 6, 'jbosnjak3@gmail.com', '2018-03-17 08:14:04'),
+(29, 0, '0:00', 1, 0, 'jbosnjak3@gmail.com', '2018-03-17 15:19:11');
 
 -- --------------------------------------------------------
 
@@ -186,6 +224,14 @@ CREATE TABLE `walkingstatistika` (
   `period` datetime NOT NULL,
   `ukupanBrojKoraka` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_croatian_ci;
+
+--
+-- Dumping data for table `walkingstatistika`
+--
+
+INSERT INTO `walkingstatistika` (`id`, `email`, `ukupnaUdaljenost`, `ukupnoVrijemeAktivnosti`, `prosjecnaBrzinaUkm`, `period`, `ukupanBrojKoraka`) VALUES
+(1, 'jbosnjak3@gmail.com', 1.7767600472443834, 55893, 2, '2018-03-17 08:14:10', 3),
+(2, 'jbosnjak3@gmail.com', 0, 19094, 0, '2018-03-17 15:19:16', 1);
 
 --
 -- Indexes for dumped tables
@@ -256,13 +302,13 @@ ALTER TABLE `walkingstatistika`
 -- AUTO_INCREMENT for table `biking`
 --
 ALTER TABLE `biking`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `bikingstatistika`
 --
 ALTER TABLE `bikingstatistika`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `registration`
@@ -274,31 +320,31 @@ ALTER TABLE `registration`
 -- AUTO_INCREMENT for table `running`
 --
 ALTER TABLE `running`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `runningstatistika`
 --
 ALTER TABLE `runningstatistika`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `summarystatistika`
 --
 ALTER TABLE `summarystatistika`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `walking`
 --
 ALTER TABLE `walking`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
 
 --
 -- AUTO_INCREMENT for table `walkingstatistika`
 --
 ALTER TABLE `walkingstatistika`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- Constraints for dumped tables
