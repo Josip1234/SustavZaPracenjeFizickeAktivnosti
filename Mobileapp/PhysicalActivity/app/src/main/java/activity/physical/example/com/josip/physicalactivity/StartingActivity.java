0881a5d7706df0ec28dc7900a09e357728c4b154
Josip Bošnjak <jbosnjak@unipu.hr>
2018-03-22 10:04:30 +0100

    sada se toast ne pojavljuje ako se aplikacija ne koristi više puta

--- Mobileapp/PhysicalActivity/app/src/main/java/activity/physical/example/com/josip/physicalactivity/StartingActivity.java
+++ Mobileapp/PhysicalActivity/app/src/main/java/activity/physical/example/com/josip/physicalactivity/StartingActivity.java
@@ -21,14 +21,101 @@ import java.io.IOException;
 public class StartingActivity extends AppCompatActivity {
     //broji broj puta koliko se aplikacija koristi
     private int brojKoristenja=0;
+    private String naziv="brojKoristenja.json";
+
+    public int brojKoristenja() throws IOException,JSONException {
+        FileInputStream fis = openFileInput(naziv);
+        if(fis==null){
+            try {
+                JSONArray polje = new JSONArray();
+                JSONObject ob = new JSONObject();
+
+                ob.put("brojKoristenja", 0);
+
+
+
+                polje.put(ob);
+                String text = polje.toString();
+                FileOutputStream fos = openFileOutput(naziv, MODE_PRIVATE);
+                fos.write(text.getBytes());
+                fos.close();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return 0;
+        }else {
+            BufferedInputStream bis = new BufferedInputStream(fis);
+            StringBuffer b = new StringBuffer();
+            while (bis.available() != 0) {
+                char c = (char) bis.read();
+                b.append(c);
+            }
+            bis.close();
+            fis.close();
+            int brojKoristenja = 0;
+            JSONArray data = new JSONArray(b.toString());
+            StringBuffer prijavaBuffer = new StringBuffer();
+            for (int i = 0; i < data.length(); i++) {
+                brojKoristenja += data.getJSONObject(i).getInt("brojKoristenja");
+
+                Log.i("poruka", "pročitan json");
+
+
+            }
+
+
+            return brojKoristenja;
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_starting);
+
         //uvecavaj broj koristenja
-        brojKoristenja+=1;
-        if(brojKoristenja==1){
+        try {
+            brojKoristenja+=brojKoristenja();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        //ako je broj korištenja jednak nuli aplikacija se koristi prvi put
+        if(brojKoristenja==0){
             Toast.makeText(this, R.string.Starting, Toast.LENGTH_LONG).show();
+            brojKoristenja+=1;
+            JSONArray polje = new JSONArray();
+            JSONObject ob = new JSONObject();
+            //uvećaj i zapamti rezultat
+            try {
+                ob.put("brojKoristenja", brojKoristenja);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+
+            polje.put(ob);
+            String text = polje.toString();
+            FileOutputStream fos = null;
+            try {
+                fos = openFileOutput(naziv, MODE_PRIVATE);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+            try {
+                fos.write(text.getBytes());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                fos.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
         }
 
     }
