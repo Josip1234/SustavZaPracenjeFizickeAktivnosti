b0cef8b42713c795572b9e1b35b349d3c4e66686
Josip Bošnjak <jbosnjak3@gmail.com>
2025-07-08 13:29:47 +0200

    update
    added arrow icons for positive negative and neutral trend weights

--- index.php
+++ index.php
@@ -10,7 +10,13 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.min.js" integrity="sha384-7qAoOXltbVP82dhxHAUje59V5r2YsVfBafyUDxEdApLPmcdhBPg1DKg1ERo0BZlK" crossorigin="anonymous"></script>
 </head>
 <body>
-        <h1>Hello, world!</h1>
+        <div class="container">
+                 <div class="row">
+                            <div class="col">
+                                <h2>Obrazac za unos težine</h2>
+                            </div>
+                 </div>
+        </div>
 
 </body>
 </html>
