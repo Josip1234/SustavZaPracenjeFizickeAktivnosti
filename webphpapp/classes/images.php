36ad6513d6c05be7cc3afdba0dd412ae40a6b9a7
Josip Bošnjak <jbosnjak3@gmail.com>
2025-07-09 08:40:47 +0200

    Create images.php
    added class with constant values for inserting image attributes alt src width and height will be added dynamically

---
+++ webphpapp/classes/images.php
@@ -0,0 +1,12 @@
+<?php 
+class Image{
+    const OPEN_IMAGE_SRC="<img src='";
+    const ADD_ALT_IMG="' alt='";
+    const SET_WIDTH="' width='";
+    const SET_HEIGHT="' height='";
+    const CLOSE_IMG="'>";
+}
+
+//<img src="img_girl.jpg" alt="Girl in a jacket" width="500" height="600">
+
+?>
