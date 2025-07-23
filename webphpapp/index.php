6437daea83275daddff4ae0303b0b656630f0b69
Josip Bošnjak <jbosnjak3@gmail.com>
2025-07-23 11:44:51 +0200

    update
    added new script history entry php will be used to enter previos measured data from the past added form and navigation navbar  added navbar in index.php

--- webphpapp/index.php
+++ webphpapp/index.php
@@ -12,6 +12,21 @@
 <script src="script/physical.js"></script>
 </head>
 <body>
+  <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Sustav za praćenje fizičke aktivnosti</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link" href="history_entry.php">Unos prošlih mjerenja</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
 <?php 
 include("classes/physical.php");
 include("classes/dbconn.php");
